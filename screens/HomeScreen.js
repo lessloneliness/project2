0fbf4d782b96d3b4fbe17defb65176d3697c7169
Matan <matanv009@gmail.com>
2022-04-24 18:04:07 +0300

    HomeScreenUpdate

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,54 +1,62 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { StyleSheet, Text,TouchableOpacity,View,Image, TextInput, SafeAreaView} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import { Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import * as ImagePicker from "expo-image-picker";
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/firestore";
-import storage from 'firebase/storage'; // 1
-
+import storage from "firebase/storage"; // 1
+import { Picker } from "@react-native-picker/picker";
 
 const HomeScreen = () => {
-  
   const navigation = useNavigation();
   const [myText, setMyText] = useState("Hello");
   const [bio, setBio] = useState("");
   const [image, setImage] = useState(null);
   const [userName, setUserName] = useState(null);
   const user = firebase.auth().currentUser; // from auth fire base
-  const userRef = firebase.firestore().collection('users').doc(user.uid); // get the uid.
-   
-  userRef.get().then(function(doc) {
-    if (doc.exists) {
-      const userNameDB =JSON.stringify(doc.data().Name.First); //his name from the data  base
-      const bioFromDb =JSON.stringify(doc.data().Bio); 
-      setUserName (userNameDB);  
-      if(bioFromDb.length>2) setBio(bioFromDb);
-      const ref = firebase.storage().ref('/'+user.uid+'.jpg'); // get the url from fire base stotage 
-       ref.getDownloadURL().then((url) => {
-         if(url!=null)
-          setImage(url);
+  const userRef = firebase.firestore().collection("users").doc(user.uid); // get the uid.
+  const [pickerValue, setPickerValue] = useState("test"); //set picker
 
-      }).catch((e)=>{console.log(e)});
+  userRef
+    .get()
+    .then(function (doc) {
+      if (doc.exists) {
+        const userNameDB = JSON.stringify(doc.data().Name.First); //his name from the data  base
+        const bioFromDb = JSON.stringify(doc.data().Bio);
+        setUserName(userNameDB);
+        if (bioFromDb.length > 2) setBio(bioFromDb);
+        const ref = firebase.storage().ref("/" + user.uid + ".jpg"); // get the url from fire base stotage
+        ref
+          .getDownloadURL()
+          .then((url) => {
+            if (url != null) setImage(url);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
 
-      console.log('user: ' + JSON.stringify(doc.data().Name));
-    } else {
-      
+        console.log("user: " + JSON.stringify(doc.data().Name));
+      } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
-
-    }
-}).catch(function(error) {
-
-    console.log("Error getting document:", error);
-});
-  
-
-
-
+      }
+    })
+    .catch(function (error) {
+      console.log("Error getting document:", error);
+    });
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -60,20 +68,18 @@ const HomeScreen = () => {
     });
 
     if (!result.cancelled) {
-      setMyText("Hello" +userName);
+      setMyText("Hello" + userName);
       setImage(result.uri);
     }
-
-
 
     //const filename = image.substring(image.lastIndexOf('/') + 1);
 
     const blob = await new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.onload = function() {
+      xhr.onload = function () {
         resolve(xhr.response);
       };
-      xhr.onerror = function() {
+      xhr.onerror = function () {
         reject(new TypeError("Network request failed"));
       };
       xhr.responseType = "blob";
@@ -83,96 +89,124 @@ const HomeScreen = () => {
     const ref = firebase
       .storage()
       .ref()
-      .child(user.uid+'.jpg');
-      
-    const task = ref.put(blob, { contentType: 'image/jpeg'});
+      .child(user.uid + ".jpg");
 
-    task.on('state_changed', 
+    const task = ref.put(blob, { contentType: "image/jpeg" });
+
+    task.on(
+      "state_changed",
       (snapshot) => {
-        console.log(snapshot.totalBytes)
-      }, 
+        console.log(snapshot.totalBytes);
+      },
       (err) => {
-        console.log(err)
-      }, 
+        console.log(err);
+      },
       () => {
         task.snapshot.ref.getDownloadURL().then((downloadURL) => {
           console.log(downloadURL);
-
-      });
-    })
-
-   
+        });
+      }
+    );
   };
 
-
-  const handleSignOut = () => {      // when the user click on sign out , fire base save it.
-    saveInDb();         // save The bio change
-    auth.signOut().then(() =>
-     { navigation.replace("Less loneless");})
+  const handleSignOut = () => {
+    // when the user click on sign out , fire base save it.
+    saveInDb(); // save The bio change
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Less loneless");
+      })
       .catch((error) => alert(error.message));
   };
 
-  const saveInDb = ()=>{
-    if(bio.length!=0){
-      firebase.firestore().collection('users').doc(user.uid).update({
-        Bio:bio
-      }).catch(error => console.log(error.message));
+  const saveInDb = () => {
+    if (bio.length != 0) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .update({
+          Bio: bio,
+        })
+        .catch((error) => console.log(error.message));
     }
-  }
-
+  };
 
   return (
-    <SafeAreaView>
-      <View style={styles.emailView}>
-        <Text> {myText}</Text>
-        <Text>{auth.currentUser?.email}</Text>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.body}></View>
-        <Text style={styles.text}>Less Loneliness</Text>
-        <Text
-          style={{
-            color: "red",
-            fontSize: 20,
-            textAlign: "center",
-            margin: 0,
-            fontStyle: "italic",
-            marginBottom:1,
-            marginTop:3
-          }}
-        >
-          Create Profile
-        </Text>
-        <Button title={"Add Photo"} onPress={pickImage}></Button>
-        <Image style={styles.Image} source={{ uri: image }}></Image>
-        <View style={{ alignItems: "center" }}></View>
-        <Text>Enter bio</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="bio details"
-          onChangeText={(val) => setBio(val)}
-        />
-        <Text>Bio:{bio}</Text>
-        <Button 
-          title={"Add User"}
-          onPress={()=>navigation.replace("Add User Screen")}   
-        ></Button>
-        <Button
-          title={"Create Group"}
-          onPress={()=>navigation.replace("groupScreen")}   
-           
-        ></Button>
-        <Button
-          title={"Create Event"}
-          onPress={()=>navigation.replace("Create Event Screen")}   
-        ></Button>
-        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-          <Text style={styles.buttonText}>Sign out</Text>
-        </TouchableOpacity>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.container}></View>
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView>
+        <View style={styles.emailView}>
+          <Text> {myText}</Text>
+          <Text>{auth.currentUser?.email}</Text>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.body}></View>
+          <Text style={styles.text}>Less Loneliness</Text>
+          <Text
+            style={{
+              color: "red",
+              fontSize: 20,
+              textAlign: "center",
+              margin: 0,
+              fontStyle: "italic",
+              marginBottom: 1,
+              marginTop: 3,
+            }}
+          >
+            Create Profile
+          </Text>
+          <Button title={"Add Photo"} onPress={pickImage}></Button>
+          <Image style={styles.Image} source={{ uri: image }}></Image>
+          <View style={{ alignItems: "center" }}></View>
+          <Text>Enter bio</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="bio details"
+            onChangeText={(val) => setBio(val)}
+          />
+          <Text>Bio:{bio}</Text>
+          <Button
+            title={"Add User"}
+            onPress={() => navigation.replace("Add User Screen")}
+          ></Button>
+          <Button
+            title={"Create Group"}
+            onPress={() => navigation.replace("groupScreen")}
+          ></Button>
+          <Button
+            title={"Create Event"}
+            onPress={() => navigation.replace("Create Event Screen")}
+          ></Button>
+          <Picker
+            style={styles.Picker}
+            selcetedValue={pickerValue}
+            onValueCahnges={(itemValue) => setPickerValue(itemValue)}
+          >
+            <Picker.Item label="My friends" value="My friends" />
+          </Picker>
+          <Picker
+            style={styles.Picker}
+            selcetedValue={pickerValue}
+            onValueCahnges={(itemValue) => setPickerValue(itemValue)}
+          >
+            <Picker.Item label="My Group" value="My Group" />
+          </Picker>
+          <Picker
+            style={styles.Picker}
+            selcetedValue={pickerValue}
+            onValueCahnges={(itemValue) => setPickerValue(itemValue)}
+          >
+            <Picker.Item label="My Events" value="My Events" />
+          </Picker>
+          <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+            <Text style={styles.buttonText}>Sign out</Text>
+          </TouchableOpacity>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}></View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -181,7 +215,7 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     width: 400,
-    height: 530,
+    height: 700,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -190,8 +224,8 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: "green",
     fontStyle: "italic",
-    marginTop:0,
-    marginBottom:10,
+    marginTop: 0,
+    marginBottom: 10,
   },
   input: {
     borderWidth: 1,
@@ -219,6 +253,12 @@ const styles = StyleSheet.create({
     color: "red",
     fontWeight: "700",
     fontSize: 16,
-    marginTop:30
-   },
+    marginTop: 20,
+  },
+  Picker: {
+    width: 140,
+    height: 45,
+    borderColor: "blue",
+    borderWidth: 1,
+  },
 });
